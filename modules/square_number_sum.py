8e21fa5a8110c3a2d004c71cdc838d1ccd91ae2b
Gearoid O'Treasaigh <gotreasa@users.noreply.github.com>
2024-02-13 15:35:09 +0000

    refactor: clear up the number using the word numbers

--- modules/square_number_sum.py
+++ modules/square_number_sum.py
@@ -1,9 +1,9 @@
-def square_sum(values: list[int]):
-    if not isinstance(values, list):
+def square_sum(numbers: list[int]):
+    if not isinstance(numbers, list):
         raise ValueError("❗️ Input should be an array")
-    if not all(isinstance(value, int) for value in values):
+    if not all(isinstance(number, int) for number in numbers):
         raise ValueError("❗️ Input should be an array of numbers")
     result = 0
-    for value in values:
-        result += value**2
+    for number in numbers:
+        result += number**2
     return result
